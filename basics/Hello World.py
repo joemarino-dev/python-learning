595a47c09840845fc9623c7c4e25afe54b0aef84
Joe Marino <joe@mistermarino.com>
2025-12-04 09:51:08 -0500

    Test updated file and commit try 3

--- basics/Hello World.py
+++ basics/Hello World.py
@@ -1,2 +1,3 @@
+# Attempting to setup pythonista to github push via a-shell mini
 print("Hello Joe! You are now pushing changes from Pythonista to GitHub. Let's goooooo!'")
 print("Will it know that I've now added a second line of code?")
