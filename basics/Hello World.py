3b8e0d324d6ef3b5617b097ace50e2da926c23b8
Joe Marino <joe@mistermarino.com>
2025-12-04 10:42:30 -0500

    Test SSH remote with lg2

--- basics/Hello World.py
+++ basics/Hello World.py
@@ -1,3 +1,4 @@
 # Attempting to setup pythonista to github push via a-shell mini
+# Another update after adding proper authentication
 print("Hello Joe! You are now pushing changes from Pythonista to GitHub. Let's goooooo!'")
 print("Will it know that I've now added a second line of code?")
